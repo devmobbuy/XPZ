85f8aff1f29a732ea6fac6ec3aee5d917e7f8023
devmobbuy <wanderleia.cruz@mobbuy.com.br>
2021-12-22 17:44:07 -0300

    Atualiza do Fix

--- Para_Liberacao/LiberaFix/autofix.sh
+++ Para_Liberacao/LiberaFix/autofix.sh
@@ -30,42 +30,50 @@ if echo "$resposta" | grep -iq "^s" ;then
 		
 		if (echo "$respostaT" | grep -iq "^01$") || (echo "$respostaT" | grep -iq "^00$") ;then
 			echo ""
-			echo "Realizando Backup banese"			
-			cp /opt/apache-tomcat-7.0.109/webapps/banese /opt/apache-tomcat-7.0.109/webapps/baneseOLD
+			echo "Realizando Backup banese"													
+			cp -fR /opt/apache-tomcat-7.0.109/webapps/banese/ /opt/apache-tomcat-7.0.109/Backup/			
 			echo "Atualizando banese"
-			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/banese/			
+			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/banese/	
+			sudo curl -u admin:MobPwd@12345 http://localhost:8087/manager/text/reload?path=/banese		
 		fi
 
 		if (echo "$respostaT" | grep -iq "^02$") || (echo "$respostaT" | grep -iq "^00$") ;then
 			echo ""
 			echo "Realizando Backup bemfacil"
-			cp /opt/apache-tomcat-7.0.109/webapps/bemfacil /opt/apache-tomcat-7.0.109/webapps/bemfacilOLD
+			cp -fR /opt/apache-tomcat-7.0.109/webapps/bemfacil/ /opt/apache-tomcat-7.0.109/Backup/
 			echo "Atualizando bemfacil"
 			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/bemfacil/
+			sudo curl -u admin:MobPwd@12345 http://localhost:8087/manager/text/reload?path=/bemfacil
 		fi
 		
 		if (echo "$respostaT" | grep -iq "^03$") || (echo "$respostaT" | grep -iq "^00$") ;then
 			echo ""
 			echo "Realizando Backup credpag"			
-			cp /opt/apache-tomcat-7.0.109/webapps/credpag /opt/apache-tomcat-7.0.109/webapps/credpagOLD
+			cp -fR /opt/apache-tomcat-7.0.109/webapps/credpag/ /opt/apache-tomcat-7.0.109/Backup/
 			echo "Atualizando credpag"
 			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/credpag/
+			
+			sudo curl -u admin:MobPwd@12345 http://localhost:8087/manager/text/reload?path=/credpag
 		fi
 		
 		if (echo "$respostaT" | grep -iq "^04$") || (echo "$respostaT" | grep -iq "^00$") ;then
 			echo ""
 			echo "Realizando Backup pronto"			
-			cp /opt/apache-tomcat-7.0.109/webapps/pronto /opt/apache-tomcat-7.0.109/webapps/prontoOLD
+			cp -fR /opt/apache-tomcat-7.0.109/webapps/pronto/ /opt/apache-tomcat-7.0.109/Backup/
 			echo "Atualizando pronto"
 			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/pronto/
+			
+			sudo curl -u admin:MobPwd@12345 http://localhost:8087/manager/text/reload?path=/pronto
 		fi
 				
 		if (echo "$respostaT" | grep -iq "^05$") || (echo "$respostaT" | grep -iq "^00$") ;then
 			echo ""
 			echo "Realizando Backup smartpagamentos"			
-			cp /opt/apache-tomcat-7.0.109/webapps/smartpagamentos /opt/apache-tomcat-7.0.109/webapps/smartpagamentosOLD
+			cp -fR /opt/apache-tomcat-7.0.109/webapps/smartpagamentos/ /opt/apache-tomcat-7.0.109/Backup/
 			echo "Atualizando smartpagamentos"
 			unzip $caminhoFix -d /opt/apache-tomcat-7.0.109/webapps/smartpagamentos/
+			
+			sudo curl -u admin:MobPwd@12345 http://localhost:8087/manager/text/reload?path=/smartpagamentos
 		fi		
 		
 		echo ""
