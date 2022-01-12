0d46909b3e82a1e3e68c04813dcd3ff36d9f3b54
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2022-01-12 17:40:07 -0300

    Add files via upload
    INSERT no ParSis, incluindo o ParCod: DIRF_ASSINATURA (TODOS BANCOS)

---
+++ Scripts/Ver_1/04_#14765_InsertDIRF_ASSINATURA_PARSIS _CarlosPatricio_12012022.sql
@@ -0,0 +1,97 @@
+/********* TAREFA #14765 - CARLOS PATRICIO - 12/01/2022 ************************/
+/********************************************************************************
+Use Banese
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/banese/static/Resources/AssinaturaDirf/assinaturaBanese.jpg',
+'012ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
+/********************************************************************************
+Use BemFacil
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/bemfacil/static/Resources/AssinaturaDirf/assinaturaBemfacil.jpg',
+'006ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
+/********************************************************************************
+Use Credinov
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/credinov/static/Resources/AssinaturaDirf/assinaturaCredinov.jpg',
+'026ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
+/********************************************************************************
+Use CredPag
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/credpag/static/Resources/AssinaturaDirf/assinaturaCredpag.jpg',
+'024ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
+/********************************************************************************
+Use Pronto
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/pronto/static/Resources/AssinaturaDirf/assinaturaPronto.jpg',
+'025ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
+/********************************************************************************
+Use SmartPagamentos
+insert into PARSIS values (
+'DIRF_ASSINATURA',
+'Imagem da assinatura do responsável por assinar a DIRF',
+'IM',
+NULL,
+NULL,
+'N',
+'/opt/apache-tomcat-7.0.109/webapps/smartpagamentos/static/Resources/AssinaturaDirf/assinaturaSmartpagamentos.jpg',
+'018ADM',
+'2022-01-12',
+NULL,
+NULL,
+0)
+***********************************************************************************/
