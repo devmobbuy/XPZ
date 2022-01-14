03adddac4fdadaabc402d8d4e0e65853a7a65cdf
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-01-14 17:08:34 -0300

    Ajuste no Script de liberação

---
+++ Scripts/Ver_2.5.5/03_#14737_INSERT_PARSIS_VENDA_REL_SINTETICO_Carlos_14012022.sql
@@ -0,0 +1,103 @@
+/********* TAREFA #14737 - CARLOS PATRICIO - 14/01/2022 ****************/
+/************************************************************************
+use SmartPagamentos
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'S',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
+/************************************************************************
+use Banese
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'N',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
+/************************************************************************
+use Bemfacil
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'N',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
+/************************************************************************
+use Credinov
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'N',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
+/************************************************************************
+use Credpag
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'N',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
+/************************************************************************
+use Pronto
+
+INSERT INTO PARSIS VALUES (
+'VENDA_REL_SINTETICO',
+'Exibe ou Oculta Checkbox Exportar Relatório Sintético na Tela Venda',
+'CA',
+1,
+NULL,
+'N',
+'N',
+'018ADM',
+'2022-01-14',
+NULL,
+NULL,
+0)
+************************************************************************/
