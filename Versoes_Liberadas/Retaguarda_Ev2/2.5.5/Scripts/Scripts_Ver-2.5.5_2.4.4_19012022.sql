2188090e4cb393dd01b6170a9f5d8c3284d4a5ea
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-01-19 17:02:42 -0300

    Script de liberação

---
+++ Versoes_Liberadas/Retaguarda_Ev2/2.5.5/Scripts/Scripts_Ver-2.5.5_2.4.4_19012022.sql
@@ -0,0 +1,59 @@
+/* TAREFA #14799 - JOSÉ */
+
+-- QUAIS CLIENTES PRECISA RODAR QUANDO ATUALIZAR A VERSÃO --
+USE Banese
+USE BemFacil
+USE Credinov
+USE CredPag
+USE Pronto
+USE SmartPagamentos
+
+-- EXCLUSÃO E CRIAÇÃO DE ÍNDICES --
+
+DROP INDEX [IVLRPAG7] ON [VLRPAG]
+
+CREATE NONCLUSTERED INDEX [UVLRPAG6] ON [VLRPAG] (
+      [VlpMovTrnId],
+      [VlpAnpNum])
+
+CREATE NONCLUSTERED INDEX [UANTPAG2] ON [ANTPAG] (
+      [AnpStsAnt])
+
+-- CRIAÇÃO DE VIEW --
+
+CREATE VIEW TxAntManualVw AS
+
+SELECT
+M.EstCod AS 'TxAntManualEstCod',
+M.MovTrnDta AS 'TxAntManualData',
+SUM(L.LaaVlrTxaAnt) AS 'TxAntManualVlr'
+
+FROM MovTrn01 M
+INNER JOIN VLRPAG V
+ON M.MovTrnId = V.VlpMovTrnId
+
+INNER JOIN LANANT L
+ON V.VlpNumLan = L.LaaNumLan
+
+INNER JOIN ANTPAG A
+ON L.AnpNumAnt = A.AnpNumAnt
+
+WHERE V.VlpAnpNum > 0 AND A.AnpStsAnt = 2
+
+GROUP BY M.EstCod, M.MovTrnDta
+
+/* ALTERAÇÃO DA VERSÃO DO BANCO */
+USE Banese
+USE Bemfacil
+USE Credinov
+USE Credpag
+USE Pronto
+USE SmartPagamentos
+
+UPDATE PARSIS
+SET ParCon = '2.5.5_2.4.4'
+WHERE ParCod = 'VERSAO_BANCO'
+
+UPDATE PARSIS
+SET ParCon = '19/01/2022'
+WHERE ParCod = 'VERSAO_BANCO_DATA'
