47c68f1160c7eeac5e98683e9369e8107c1aae7e
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-01-21 09:51:23 -0300

    02_#14812_CriacaoCampoEstSegmento_Jose_20012022

---
+++ Scripts/Ver_QA/02_#14812_CriacaoCampoEstSegmento_Jose_20012022.sql
@@ -0,0 +1,8 @@
+/* TAREFA #14812 - JOSÉ */
+
+--Criação do campo EstSegmento
+ALTER TABLE [EST]
+ADD [EstSegmento] VARCHAR(40)     NOT NULL CONSTRAINT EstSegmentoEST_DEFAULT DEFAULT ''
+
+ALTER TABLE [EST]
+DROP CONSTRAINT EstSegmentoEST_DEFAULT
