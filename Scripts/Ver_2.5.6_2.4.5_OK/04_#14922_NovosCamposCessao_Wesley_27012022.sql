61378f95cec9a7195e86a7d09a9feb188a734545
wesleyEmidio <wesley_esc1987@hotmail.com>
2022-01-27 11:40:28 -0300

    Novos campos da tabela cessao

---
+++ Scripts/Ver_2.5.6_2.4.5_OK/04_#14922_NovosCamposCessao_Wesley_27012022.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE [CESSAO] 
+ADD [CessaoFilial] VARCHAR(128)    NULL, 
+    [CessaoDistribuidor] VARCHAR(128)    NULL, 
+    [CessaoDataEmissao] DATETIME    NULL, 
+    [CessaoValorNota] DECIMAL(17,2)    NULL
