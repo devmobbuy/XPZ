9e4cffdbed06e998866010e91985a654cbf10e5f
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-02-10 15:48:09 -0300

    Ajuste robo

--- sh_robos/sh_banese/fechamento_mensal.sh
+++ sh_robos/sh_banese/fechamento_mensal.sh
@@ -1,4 +1,4 @@
-cd /usr/local/tomcat/webapps/banese/WEB-INF/classes
-/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin/java  -classpath ../lib/commons-lang-2.4.jar:../lib/gxclassR.jar:../lib/Una2000.jar:../lib/iText.jar:../lib:.:../lib/gxclassR.jar:../lib/jtds-1.2.jar:../lib/ojdbc14.jar:.: rpt0002_fec
+cd /opt/apache-tomcat-7.0.109/webapps/banese/WEB-INF/classes
+/opt/jdk1.8.0_291/bin/java  -classpath ../lib/commons-lang-2.4.jar:../lib/gxclassR.jar:../lib/Una2000.jar:../lib/iText.jar:../lib:.:../lib/gxclassR.jar:../lib/jtds-1.2.jar:../lib/ojdbc14.jar:.: rpt0002_fec
 
 
