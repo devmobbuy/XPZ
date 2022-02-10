147c8d68545b1fdc1fa346876fc4b1d15712895f
devmobbuy <wanderleia.cruz@mobbuy.com.br>
2022-02-10 19:14:03 -0300

    Ver-2.5.9

---
+++ Scripts/Ver_2.5.9/05_#14868_IndexCancelamentoEdi_Wesley_10022022.sql
@@ -0,0 +1,4 @@
+CREATE NONCLUSTERED INDEX [UTRN05] ON [TRN05] (
+      [TrnBrsRdeSit],
+      [TrnBrsOprSitPrc],
+      [TrnBrsFecSeqArq])
--- Scripts/Ver_QA/05_#14868_IndexCancelamentoEdi_Wesley_10022022.sql
+++
@@ -1,4 +0,0 @@
-CREATE NONCLUSTERED INDEX [UTRN05] ON [TRN05] (
-      [TrnBrsRdeSit],
-      [TrnBrsOprSitPrc],
-      [TrnBrsFecSeqArq])
