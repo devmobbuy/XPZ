52ac02f71068cae1f68e912e972a0f5897456a1d
devmobbuy <wanderleia.cruz@mobbuy.com.br>
2022-03-24 16:22:54 -0300

    16786

--- Scripts/Ver_QA/01_#16786_VerificarconcorrenciadeprocessosantecipaçãoXArquivodebanco_Léia_17032022.sql
+++ Scripts/Ver_QA/01_#16786_VerificarconcorrenciadeprocessosantecipaçãoXArquivodebanco_Léia_17032022.sql
@@ -2,7 +2,7 @@
 /* TAREFA #16786 - LÉIA */  
 
 ALTER TABLE [Processo] ADD [ProcessoClasse] VARCHAR(45) NULL;
-
+ALTER TABLE [Processo] ADD [ProcessoOrigem] VARCHAR(50) NULL;
 
 CREATE NONCLUSTERED INDEX [UCNTBAN] ON [CNTBAN] ([CntFlgDOC]);
 
