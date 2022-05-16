64d8f5532ad6773d7978c6deaf0978e849ff255c
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-05-16 09:43:25 -0300

    Ajuste de script para produção

--- Versoes_Liberadas/Retaguarda_Ev2/2.6.7/Scripts/SQL_Ver-2.6.7_2.4.15.sql
+++ Versoes_Liberadas/Retaguarda_Ev2/2.6.7/Scripts/SQL_Ver-2.6.7_2.4.15.sql
@@ -54,3 +54,20 @@ SELECT * FROM sse2_ung
 --Rodar quarto
 INSERT INTO sse2_grp_mod
 VALUES (25, 'ADM', 'HRPT0200_GRID') --Dar o SELECT de cima na sse2_ung e pagar o UNG2Cod e alterar conforme o valor de cada cliente
+
+/* TAREFA #17436 - LÉIA */
+
+CREATE NONCLUSTERED INDEX [UVAN023] ON [VAN02] (
+      [VanCod],
+      [VanWbsDat],
+      [VanWbsSeq] DESC)
+	  
+CREATE NONCLUSTERED INDEX [UVAN024] ON [VAN02] (
+      [VanWbsDat] DESC,
+      [VanWbsSta])
+
+CREATE NONCLUSTERED INDEX [UVAN025] ON [VAN02] (
+      [VanWbsDat],
+      [VanWbsNumSer],
+      [VanWbsNumCrt],
+      [VanWbsVlr])
