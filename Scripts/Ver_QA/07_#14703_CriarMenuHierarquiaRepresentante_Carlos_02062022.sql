990185ad7c03afd6e5372a7421d42d66b7acc17e
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2022-06-02 16:15:13 -0300

    Add files via upload

---
+++ Scripts/Ver_QA/07_#14703_CriarMenuHierarquiaRepresentante_Carlos_02062022.sql
@@ -0,0 +1,104 @@
+/*
+USE BEMFACIL
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/bemfacil/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (6, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (6, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE BANESE
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/banese/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (12, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (12, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE CREDINOV
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/credinov/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (26, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (26, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE CREDPAG
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/credpag/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (24, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (24, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE SMARTPAGAMENTOS
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/smartpagamentos/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (18, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (18, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE PRONTO
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/pronto/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (25, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (25, 'ADM', 'wpHierarquia_Grid')
+*/
+
+/*
+USE THEPAY
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpHierarquia_Grid', 'wpHierarquia_Grid', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpHierarquia_Grid', '', 10, 'CRED_ESTAB', '', '/thepay/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (27, 'wpHierarquia_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (27, 'ADM', 'wpHierarquia_Grid')
+*/
