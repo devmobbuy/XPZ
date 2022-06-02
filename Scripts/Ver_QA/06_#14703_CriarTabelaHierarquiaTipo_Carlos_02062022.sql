abb4b29b92bbeb1f4fe6431a22bd648130d0b4eb
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2022-06-02 16:34:52 -0300

    Delete 06_#14703_CriarTabelaHierarquiaTipo_Carlos_02062022.sql

--- Scripts/Ver_QA/06_#14703_CriarTabelaHierarquiaTipo_Carlos_02062022.sql
+++
@@ -1,9 +0,0 @@
-use bemfacil
-/*CRIA A TABELA HierarquiaTipo*/
-/*
-CREATE TABLE [HierarquiaTipo] (
-  [HierarquiaTipoId]   DECIMAL(10)    NOT NULL,
-  [HierarquiaTipoDesc] VARCHAR(255)    NOT NULL,
-     PRIMARY KEY ( [HierarquiaTipoId] ))
-
-*/
