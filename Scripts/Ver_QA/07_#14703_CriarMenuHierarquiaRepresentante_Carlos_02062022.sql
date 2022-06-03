c94db083a36eccd7bfc9c66e3c9191ff48ba8214
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2022-06-03 14:11:57 -0300

    Add files via upload

---
+++ Scripts/Ver_QA/07_#14703_CriarMenuHierarquiaRepresentante_Carlos_02062022.sql
@@ -0,0 +1,104 @@
+/*
+USE BEMFACIL
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/bemfacil/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (6, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (6, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE BANESE
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/banese/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (12, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (12, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE CREDINOV
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/credinov/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (26, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (26, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE CREDPAG
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/credpag/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (24, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (24, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE SMARTPAGAMENTOS
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/smartpagamentos/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (18, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (218, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE PRONTO
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/pronto/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (25, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (25, 'ADM', 'WPHIERARQUIA_GRID')
+*/
+
+/*
+USE THEPAY
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('WPHIERARQUIA_GRID', 'WPHIERARQUIA_GRID', 'Hierarquia Representantes', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'WPHIERARQUIA_GRID', '', 110, 'CRED_ESTAB', 'Hierarquia Representantes', '/thepay/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (27, 'WPHIERARQUIA_GRID')
+
+INSERT INTO sse2_grp_mod
+VALUES (27, 'ADM', 'WPHIERARQUIA_GRID')
+*/
