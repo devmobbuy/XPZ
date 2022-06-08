87edc3a8364e5a7f4a65bfe15d3806eeba2cc1db
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-06-08 16:41:27 -0300

    12_#18061_AlteracaoEstIETamanho_Jose_08062022.sql

---
+++ Scripts/Ver_QA/12_#18061_AlteracaoEstIETamanho_Jose_08062022.sql
@@ -0,0 +1,12 @@
+/* TAREFA #18061 - JOSÉ */
+
+--CLientes que irão usar o script
+USE Banese
+USE BemFacil
+USE Credinov
+USE CredPag
+USE Pronto
+USE SmartPagamentos
+
+ALTER TABLE [EST]
+ALTER COLUMN [EstIE] DECIMAL(15) NULL
