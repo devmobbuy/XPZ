aa0e39e80049fef970aebf2f961cca35b2eadf24
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2022-07-07 10:38:34 -0300

    Add files via upload

---
+++ Scripts/Ver_QA/07_#14703_CriaParSisBotaoTxComissao_Carlos_07072022.sql
@@ -0,0 +1,8 @@
+--use Banese
+--use BemFacil
+--use Smartpagamentos
+--use CredInov
+--use CredPag
+--use Pronto
+--use ThePay
+INSERT INTO PARSIS VALUES ('BOTAO_TAXA_COMISSOES','Exibe ou oculta botão Comissões na tela de Tabela de Planos','CA',1,NULL,'N','N','018ADM','2022-07-07',NULL,NULL,0)
