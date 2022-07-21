0e6dd2e14ff59a2032362b89e099215b4e7c39a2
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-07-21 15:51:35 -0300

    Organização de scripts liberados

---
+++ Versoes_Liberadas/Scripts/SQL_Ver_2.4.22_3.3.4.sql
@@ -0,0 +1,44 @@
+/* TAREFA #18510 - JOSÉ */
+
+--Rodar em todos os bancos
+CREATE NONCLUSTERED INDEX [UARQDET6] ON [ARQDET] (
+      [ArbDetDtaCre] DESC,
+      [ArbDetEstCod],
+      [ArbDetCodSit])
+	  
+CREATE NONCLUSTERED INDEX [UCESSAO7] ON [Cessao] (
+      [CessaoDtaLiq] DESC,
+      [CessaoEstCodBen],
+      [CessaoStatusId])
+
+/* TAREFA #18484 - WESLEY */
+
+CREATE NONCLUSTERED INDEX [UTABVEN05] ON [TABVEN05] (
+      [TavNum],
+      [TavAdqCod],
+      [TavVigDtaIni] DESC)
+
+USE PRONTO
+
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('LogTaxas_Grid', 'LogTaxas_Grid', 'Log das taxas', '', '', 0, 1, '')
+GO
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'LogTaxas_Grid', '', 103, 'AUDITORIA', '', '/pronto/servlet/')
+GO
+
+-- 25 é PRONTO, VERIFICAR O UNG2COD DE CADA CLIENTE ANTES DE RODAR
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (25, 'LogTaxas_Grid')
+GO
+
+--Insere tela no grupo
+INSERT INTO sse2_grp_mod
+VALUES (25, 'COMPLIANCE', 'LogTaxas_Grid')
+GO
+
+--Insere tela no grupo
+INSERT INTO sse2_grp_mod
+VALUES (25, 'FINANCEIRO', 'LogTaxas_Grid')
+GO
