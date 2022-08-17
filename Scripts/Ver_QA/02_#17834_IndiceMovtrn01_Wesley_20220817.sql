164015bbd5f160af891e5111a0e52d2e45061e5f
wesleyEmidio <wesley_esc1987@hotmail.com>
2022-08-17 15:59:48 -0300

    indices

---
+++ Scripts/Ver_QA/02_#17834_IndiceMovtrn01_Wesley_20220817.sql
@@ -0,0 +1,7 @@
+CREATE NONCLUSTERED INDEX [UMOVTRN030] ON [MovTrn01] (
+      [MovTrnCod],
+      [MovTrnInsTimStp])
+ 
+CREATE NONCLUSTERED INDEX [UMOVTRN031] ON [MovTrn01] (
+      [MovTrnNsu],
+      [MovTrnDta])
