9259c7f428486ce93fc0d809dd6ec21bf419a314
JoseLLS <jose.luiz.lemos19@gmail.com>
2022-08-24 09:02:26 -0300

    Liberação de scripts para produção e organização

---
+++ Versoes_Liberadas/Scripts/SQL_Ver_2.4.25_3.3.12.sql
@@ -0,0 +1,9 @@
+/* TAREFA #17834 - WESLEY */
+
+CREATE NONCLUSTERED INDEX [UMOVTRN030] ON [MovTrn01] (
+      [MovTrnCod],
+      [MovTrnInsTimStp])
+ 
+CREATE NONCLUSTERED INDEX [UMOVTRN031] ON [MovTrn01] (
+      [MovTrnNsu],
+      [MovTrnDta])
