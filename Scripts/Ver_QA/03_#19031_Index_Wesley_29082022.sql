c108c15e5c48d70cad1bc92bd36e7f96c8e33149
wesleyEmidio <wesley_esc1987@hotmail.com>
2022-08-29 16:47:22 -0300

    index

---
+++ Scripts/Ver_QA/03_#19031_Index_Wesley_29082022.sql
@@ -0,0 +1,7 @@
+CREATE NONCLUSTERED INDEX [UEST3] ON [EST] ( 
+      [EstPacCod])
+
+CREATE NONCLUSTERED INDEX [UMOVTRN016] ON [MOVTRN01] ( 
+      [MovTrnPacCod], 
+      [EstCod], 
+      [MovTrnDta])
