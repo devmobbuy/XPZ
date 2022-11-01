7567b472ab26c6d94a3aa21b792c014d1e5cc47b
José Luiz Lemos da Silva <jose@dngx.com.br>
2022-11-01 15:26:02 -0300

    Novo script

--- Scripts/Ver_QA/05_SemTarefa_ParametroDeSudoAmbiente_Jose_31102022.sql
+++ Scripts/Ver_QA/05_SemTarefa_ParametroDeSudoAmbiente_Jose_31102022.sql
@@ -1,3 +1,5 @@
+/* SEM TAREFA - JOSÉ */
+
 INSERT INTO PARSIS
 VALUES ('SUDO_ATIVADO', 'Verifica se o sudo está habilitado no ambiente ou não para rodar robôs', 'VA', 1, NULL, 'N', 
 'S', 'ADMIN', '2022-10-31 00:00:00', NULL, NULL, 0)
