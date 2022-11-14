11d33dc34c01bde0e9381acdcc43dd28c4321303
José Luiz Lemos da Silva <jose@dngx.com.br>
2022-11-14 17:00:51 -0300

    Novo script

---
+++ Scripts/Ver_QA/03_#19972_CamponovoEst002_Jose_14112022.sql
@@ -0,0 +1,8 @@
+/* TAREFA #19972 - JOSÉ */
+
+--Rodar script em todos os clientes atualizados
+ALTER TABLE [Est002]
+ADD [EstDocExt] CHAR(10)    NOT NULL CONSTRAINT EstDocExtEst002_DEFAULT DEFAULT ''
+
+ALTER TABLE [Est002]
+DROP CONSTRAINT EstDocExtEst002_DEFAULT
