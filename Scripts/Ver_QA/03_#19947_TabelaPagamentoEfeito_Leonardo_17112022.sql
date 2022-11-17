92a6e34ad46b68a2060e748179dc49c8f6143c3f
lvsantosdev <ldsantos.developer@gmail.com>
2022-11-17 10:19:42 -0300

    tabela de pagamento de efeito

---
+++ Scripts/Ver_QA/03_#19947_TabelaPagamentoEfeito_Leonardo_17112022.sql
@@ -0,0 +1,28 @@
+
+
+CREATE TABLE [dbo].[PagamentoEfeito](
+	[PagamentoEfeitoEstCod] [int] NOT NULL,
+	[PagamentoEfeitoBeneficiario] [varchar](15) NULL,
+	[PagamentoEfeitoNrConta] [varchar](20) NULL,
+	[PagamentoEfeitoDigConta] [varchar](1) NULL,
+	[PagamentoEfeitoAgencia] [varchar](20) NULL,
+	[PagamentoEfeitoISPB] [varchar](20) NULL,
+	[PagamentoEfeitoCompe] [varchar](20) NULL,
+	[PagamentoEfeitoDataAlt] DATETIME    NULL,
+    [PagamentoEfeitoDataInc] DATETIME    NULL
+PRIMARY KEY CLUSTERED 
+(
+	[PagamentoEfeitoEstCod] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+/*RODAR APENAS PARA BANESE*/
+USE BANESE
+GO
+
+ALTER TABLE [PagamentoEfeito]
+ADD [PagamentoEfeitoDataAlt] DATETIME    NULL,
+    [PagamentoEfeitoDataInc] DATETIME    NULL
