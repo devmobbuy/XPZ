7e4440a1fca4d3242d7ed9446503120b7fdc0333
lvsantosdev <ldsantos.developer@gmail.com>
2023-01-02 15:14:54 -0300

    alteracao procedure auditoria banese considerar lancamentos ajustes

---
+++ Scripts/Auditoria/Producao/Banese/V2 - 02012023/ProcAuditoriaBanese.sql
@@ -0,0 +1,345 @@
+USE [Banese]
+GO
+
+/****** Object:  StoredProcedure [dbo].[RELTEMPAUDITORIA]    Script Date: 02/01/2023 12:22:24 ******/
+DROP PROCEDURE [dbo].[RELTEMPAUDITORIA]
+GO
+
+/****** Object:  StoredProcedure [dbo].[RELTEMPAUDITORIA]    Script Date: 02/01/2023 12:22:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[RELTEMPAUDITORIA](@ESTCOD as integer, @UsrId as Varchar(20), @RELGUID VARCHAR(100), @DATAINC DATETIME)
+AS
+BEGIN
+
+DELETE FROM RtPago WHERE RtPagoEstCod = @ESTCOD;
+DELETE FROM RtPagoBnf WHERE RtPagoBnfEstCod = @ESTCOD;
+DELETE FROM RtAberto WHERE RtAbertoEstCod = @ESTCOD;
+DELETE FROM RtAjuste WHERE RTAJUSTEESTCOD = @ESTCOD;
+DELETE FROM RTANALITICO WHERE RTANALITICOESTCOD = @ESTCOD;
+DELETE FROM RTCONSOLIDADO WHERE RTCONSOLIDADOESTCOD = @ESTCOD;
+DELETE FROM RTCANCELADO WHERE RTCANCELADOESTCOD = @ESTCOD;
+DELETE FROM RTCONCILIADO WHERE RTCONCILIADOESTCOD = @ESTCOD;
+DELETE FROM RtAntecipacao WHERE RtAntecipacaoEstCod = @ESTCOD;
+
+DECLARE @MOVTRNVLRVENDA NUMERIC(18,2);
+DECLARE @MOVTRNVLRLIQVENDA NUMERIC(18,2);
+DECLARE @MOVTRNVLRCANC NUMERIC(18,2);
+DECLARE @MOVTRNVLRLIQCANC NUMERIC(18,2);
+
+DECLARE @TOTALANT NUMERIC(18,2);
+DECLARE @TOTALABERTO NUMERIC(18,2);
+DECLARE @TOTALCESSAOORI NUMERIC(18,2);
+DECLARE @TOTALCESSAOBEN NUMERIC(18,2);
+DECLARE @TOTALDETCESSAOORI NUMERIC(18,2);
+DECLARE @TOTALDETCESSAOBEN NUMERIC(18,2);
+DECLARE @TOTALARQUIVO NUMERIC(18,2);
+DECLARE @TOTALPAGO NUMERIC(18,2);
+DECLARE @TOTALPAGOBNF NUMERIC(18,2);
+
+/*ANALITICO VALOR PAGO ESTCOD E BENEFICIARIO IGUAIS - INICIO*/
+INSERT INTO RtPago (RtPagoEstCod, RtPagoNsu, RtPagoAutCod, RtPagoDataTrn, RtPagoProduto,
+RtPagoBandeira, RtPagoTrnCod, RtPagoValorTrn, RtPagoValorLIQEST, RtPagoValor, RtPagoDataInc, RtPagoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, A.MovTrnTipPrd, 
+A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORPAGOESTBNF, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORPAGOESTBNF, VlpNsu, VlpAutCodStr, VlpTrnCod 
+FROM VLRPAG
+WHERE ESTCOD = @ESTCOD 
+AND VlpBnfCod = @ESTCOD
+AND VlpStspag = 2
+AND VlpNsu > 0
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO VALOR PAGO ESTCOD E BENEFICIARIO IGUAIS - FIM*/
+
+/*ANALITICO VALOR PAGO ESTCOD DIFERENTE E BENEFICIARIO IGUAL - INICIO*/
+INSERT INTO RTPAGOBNF (RtPagoBNFEstCod, RtPagoBNFNsu, RtPagoBNFAutCod, RtPagoBNFDataTrn, RtPagoBNFProduto,
+RtPagoBNFBandeira, RtPagoBNFTrnCod, RtPagoBNFValorTrn, RtPagoBNFValorLIQEST, RtPagoBNFValor, RtPagoBNFDataInc, RtPagoBNFGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, A.MovTrnTipPrd, 
+A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORPAGOBNF, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORPAGOBNF, VlpNsu, VlpAutCodStr, VlpTrnCod
+FROM VLRPAG
+WHERE VlpBnfCod = @ESTCOD AND EstCod <> @ESTCOD
+AND VlpStspag = 2
+AND VlpNsu > 0
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO VALOR PAGO ESTCOD DIFERENTE E BENEFICIARIO IGUAL - FIM*/
+
+/*ANALITICO VALOR PAGO COM AJUSTE (V2) - INICIO*/
+INSERT INTO RtPago (RtPagoEstCod, RtPagoNsu, RtPagoAutCod, RtPagoDataTrn, RtPagoProduto,
+RtPagoBandeira, RtPagoTrnCod, RtPagoValorTrn, RtPagoValorLIQEST, RtPagoValor, RtPagoDataInc, RtPagoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORPAGO, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORPAGO, VlpNsu, VlpAutCodStr, VlpTrnCod FROM VLRPAG
+WHERE VLPBNFCOD = @ESTCOD
+AND ESTCOD = @ESTCOD
+AND VlpStspag = 2
+AND VlpNsu > 0
+AND VLPTRNCOD = 'AJ'
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod <> B.VlpTrnCod;
+/*ANALITICO VALOR PAGO COM AJUSTE (V2) - FIM*/
+
+/*ANALITICO VALOR PAGO ABERTO - INICIO*/
+INSERT INTO RtAberto (RtAbertoEstCod, RtAbertoNsu, RtAbertoAutCod, RtAbertoDataTrn, RtAbertoProduto, RtAbertoBandeira,
+RtAbertoTrnCod, RtAbertoValorTrn, RtAbertoValorLIQEST, RtAbertoValor, RtAbertoDataInc, RtAbertoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORABERTO, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORABERTO, VlpNsu, VlpAutCodStr, VlpTrnCod FROM VLRPAG
+WHERE VLPBNFCOD = @ESTCOD
+AND VlpStspag = 1
+AND VlpNsu > 0
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO VALOR PAGO ABERTO - FIM*/
+
+/*ANALITICO VALOR ABERTO COM AJUSTE (V2) - INICIO*/
+INSERT INTO RtAberto (RtAbertoEstCod, RtAbertoNsu, RtAbertoAutCod, RtAbertoDataTrn, RtAbertoProduto, RtAbertoBandeira,
+RtAbertoTrnCod, RtAbertoValorTrn, RtAbertoValorLIQEST, RtAbertoValor, RtAbertoDataInc, RtAbertoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORABERTO, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORABERTO, VlpNsu, VlpAutCodStr, VlpTrnCod FROM VLRPAG
+WHERE VLPBNFCOD = @ESTCOD
+AND VlpStspag = 1
+AND VlpNsu > 0
+AND VLPTRNCOD = 'AJ'
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod <> B.VlpTrnCod;
+/*ANALITICO VALOR ABERTO COM AJUSTE (V2) - FIM*/
+
+/*ANALITICO VALOR CANCELADO - INICIO*/
+INSERT INTO RtCancelado (RtCanceladoEstCod, RtCanceladoNsu, RtCanceladoAutCod, RtCanceladoDataTrn, RtCanceladoProduto,
+RtCanceladoBandeira, RtCanceladoTrnCod, RtCanceladoValorTrn, RtCanceladoValorLIQEST, 
+RtCanceladoValor, RtCanceladoDataInc, RtCanceladoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORCANCELADO, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN
+(SELECT SUM(VlpVlrPag) VALORCANCELADO, VlpNsu, VlpAutCodStr, VlpTrnCod FROM VLRPAG
+WHERE VLPBNFCOD = @ESTCOD
+AND VlpStspag IN (3, 9)
+AND VlpNsu > 0
+GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO VALOR CANCELADO - FIM*/
+
+/*ANALITICO VALOR CONCILIADO - INICIO*/
+INSERT INTO [dbo].[RtConciliado]
+	([RtConciliadoEstCod]
+	,[RtConciliadoNsu]
+	,[RtConciliadoAutCod]
+	,[RtConciliadoDataTrn]
+	,[RtConciliadoProduto]
+	,[RtConciliadoBandeira]
+	,[RtConciliadoTrnCod]
+	,[RtConciliadoValorTrn]
+	,[RtConciliadoValorLiqEst]
+	,[RtConciliadoValor]
+	,[RtConciliadoDataInc]
+	,[RtConciliadoGuid])
+	SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+	A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALOR, GETDATE(), ''
+	FROM MovTrn01 A
+	INNER JOIN
+	(SELECT SUM(VlpVlrPag) VALOR, VlpNsu, VlpAutCodStr, VlpTrnCod FROM VLRPAG
+	WHERE VLPBNFCOD = @ESTCOD
+	AND VlpStspag = 6
+	AND VlpNsu > 0
+	AND (VlpIdCreditTransaction = 0 OR VlpIdCreditTransaction IS NULL)
+	AND (VlpIdCreditTransactionPai = 0 OR VlpIdCreditTransactionPai IS NULL)
+	GROUP BY VlpNsu, VlpAutCodStr, VlpTrnCod) B
+	ON A.MovTrnNsu = B.VlpNsu
+	AND A.MovTrnAutCodStr = B.VlpAutCodStr
+	AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO VALOR CONCILIADO - FIM*/
+
+
+/*ANALITICO LANÇAMENTO DE ANTECIPACAO - INICIO*/
+INSERT INTO RtAntecipacao (RtAntecipacaoEstCod, RtAntecipacaoNsu, RtAntecipacaoAutCod, RtAntecipacaoDataTrn,
+RtAntecipacaoProduto, RtAntecipacaoBandeira, RtAntecipacaoTrnCod, RtAntecipacaoValorTrn, RtAntecipacaoValorLiqEst,
+RtAntecipacaoValor, RtAntecipacaoDataInc, RtAntecipacaoGuid)
+SELECT @ESTCOD, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 
+A.MovTrnTipPrd, A.MovTrnBan, A.MovTrnCod, A.MovTrnVlr, A.MovTrnVlrLiqEst, B.CUSTOANTECIPACAO, @DATAINC, @RELGUID
+FROM MovTrn01 A
+INNER JOIN (
+SELECT SUM(LaaVlrTxaAnt) CUSTOANTECIPACAO, B.VlpNsu, B.VlpAutCodStr, B.VlpTrnCod
+FROM LANANT A
+INNER JOIN 
+VLRPAG B
+ON A.LaaNumLan = B.VlpNumLan
+WHERE AnpNumAnt IN (
+SELECT AnpNumAnt FROM ANTPAG
+WHERE AnpCodEst = @ESTCOD
+AND AnpStsAnt = 2)
+GROUP BY B.VlpNsu, B.VlpAutCodStr, B.VlpTrnCod) B
+ON A.MovTrnNsu = B.VlpNsu
+AND A.MovTrnAutCodStr = B.VlpAutCodStr
+AND A.MovTrnCod = B.VlpTrnCod;
+/*ANALITICO LANÇAMENTO DE ANTECIPACAO - FIM*/
+
+/*INSERT ANALITICO AGRUPADO - INICIO*/
+
+DECLARE @T1 TABLE (
+	NSU NUMERIC(18,0),
+	AUT VARCHAR(20),
+	TRNCOD VARCHAR(10),
+	VALORABERTO NUMERIC(18,2),
+	VALORPAGO NUMERIC(18,2),
+	VALORCESSAO NUMERIC(18,2),
+	VALORCESSAOBNF NUMERIC(18,2),
+	CUSTOANT NUMERIC(18,2),
+	VALORCANCELADO NUMERIC(18,2),
+	VALORPAGOBNF NUMERIC(18,2),
+	VALORCONC NUMERIC(18,2)
+)
+
+INSERT INTO @T1 (NSU, AUT, TRNCOD, VALORCESSAOBNF, VALORCESSAO, VALORPAGO, VALORPAGOBNF, VALORABERTO, VALORCANCELADO, CUSTOANT, VALORCONC)
+SELECT RtPagoNsu, RtPagoAutCod, RtPagoTrnCod,0, 0, RtPagoValor, 0,0,0,0,0 
+FROM RtPago
+WHERE RtPagoEstCod = @ESTCOD
+UNION ALL
+SELECT RTPAGOBNFNSU, RTPAGOBNFAUTCOD, RtPagoBnfTrnCod,0,0,0,RTPAGOBNFVALOR,0,0,0,0 
+FROM RtPagoBnf
+WHERE RtPagoBnfEstCod = @ESTCOD
+UNION ALL
+SELECT RTABERTONSU, RTABERTOAUTCOD, RtAbertoTrnCod,0, 0, 0, 0, RTABERTOVALOR,0,0,0 
+FROM RtAberto
+WHERE RtAbertoEstCod = @ESTCOD
+UNION ALL
+SELECT RtCanceladoNsu, RtCanceladoAutCod, RtCanceladoTrnCod,0, 0, 0, 0, 0, RtCanceladoValor,0,0 
+FROM RTCANCELADO
+WHERE RtCanceladoEstCod = @ESTCOD
+UNION ALL
+SELECT RTANTECIPACAONSU, RTANTECIPACAOAUTCOD, RtAntecipacaoTrnCod,0,0,0,0,0,0, RTANTECIPACAOVALOR,0 
+FROM RtAntecipacao
+WHERE RtAntecipacaoEstCod = @ESTCOD
+UNION ALL
+SELECT RtConciliadoNsu, RtConciliadoAutCod, RtConciliadoTrnCod,0,0,0,0,0,0,0,RtConciliadoValor 
+FROM RtConciliado
+WHERE RtConciliadoEstCod = @ESTCOD;
+
+INSERT INTO RtAnalitico (RtAnaliticoEstCod, RtAnaliticoNsu, RtAnaliticoAutcod, RtAnaliticoDataTrn, RtAnaliticoProduto,
+RtAnaliticoBandeira, RtAnaliticoTrnCod, RtAnaliticoValorTrn, RtAnaliticoValorLiqEst, RtAnaliticoValorPago,
+RtAnaliticoValorAberto, RtAnaliticoValorCancelado, RtAnaliticoCessao, RtAnaliticoValorAnt, RtAnaliticoCessaoBNF,
+RtAnaliticoValorPagoBNF, RtAnaliticoValorConc, RtAnaliticoUsr, RtAnaliticoDataInc, RtAnaliticoGuid)
+SELECT 
+@ESTCOD, NSU, AUT, B.MovTrnDta, 
+IIF(SUM(VALORCESSAOBNF) > 0, 'Cessao', B.MovTrnTipPrd) PRODUTO, 
+B.MovTrnBan, B.MovTrnCod, B.MovTrnVlr, 
+IIF(SUM(VALORCESSAOBNF) > 0, SUM(VALORCESSAOBNF), B.MovTrnVlrLiqEst) LIQEST,
+SUM(VALORPAGO) PAGO,
+SUM(VALORABERTO) ABERTO,
+SUM(VALORCANCELADO) CANCELADO,
+SUM(VALORCESSAO) CESSAOORI,
+SUM(CUSTOANT) CUSTOANT,
+SUM(VALORCESSAOBNF) CESSAOBNF,
+SUM(VALORPAGOBNF) PAGOBNF,
+SUM(VALORCONC) CONC,
+@UsrId,
+@DATAINC, 
+@RELGUID
+FROM @T1 A
+INNER JOIN MovTrn01 B
+ON A.NSU = B.MovTrnNsu
+AND A.AUT = B.MovTrnAutCodStr
+AND A.TRNCOD = B.MovTrnCod
+WHERE NOT NSU IS NULL AND NOT AUT IS NULL
+GROUP BY NSU, AUT, B.MovTrnDta, B.MovTrnVlr, B.MovTrnTipPrd, B.MovTrnBan, B.MovTrnCod, B.MovTrnVlrLiqEst;
+
+/*INSERT ANALITICO AGRUPADO - FIM*/
+
+/*INSERT ANALITICO AJUSTES - INICIO*/
+INSERT INTO RtAnalitico (RtAnaliticoEstCod, RtAnaliticoNsu, RtAnaliticoAutcod, RtAnaliticoDataTrn, RtAnaliticoProduto,
+RtAnaliticoBandeira, RtAnaliticoTrnCod, RtAnaliticoValorTrn, RtAnaliticoValorLiqEst, RtAnaliticoValorPago,
+RtAnaliticoValorAberto, RtAnaliticoValorCancelado, RtAnaliticoCessao, RtAnaliticoValorAnt, RtAnaliticoCessaoBNF,
+RtAnaliticoValorPagoBNF, RtAnaliticoValorConc, RtAnaliticoUsr, RtAnaliticoDataInc, RtAnaliticoGuid)
+SELECT A.EstCod, A.MovTrnNsu, A.MovTrnAutCodStr, A.MovTrnDta, 'AJUSTE', A.MovTrnBan, A.MovTrnCod,
+A.MovTrnVlr, A.MovTrnVlrLiqEst, B.VALORPAGO, B.VALORABERTO, B.VALORCANCELADO,
+B.VALORCESSAOORI, B.CUSTOANT, B.VALORCESSAOBNF, B.PAGOBNF, B.VALORCONCILIADO, @UsrId, @DATAINC, @RELGUID
+FROM MovTrn01 A INNER JOIN (
+SELECT VLPMOVTRNID,
+IIF(VlpStspag = 2, SUM(VlpVlrPag), 0) VALORPAGO,
+IIF(VlpStspag = 1, SUM(VlpVlrPag), 0) VALORABERTO,
+IIF(VlpStspag = 3, SUM(VlpVlrPag), IIF(VlpStspag = 9, SUM(VlpVlrPag), 0)) VALORCANCELADO,
+IIF(VlpStspag = 6, SUM(VlpVlrPag), 0) VALORCONCILIADO,
+IIF(VlpStspag = 6 AND VLPIDCREDITTRANSACTION > 0, SUM(VlpVlrPag), 0) VALORCESSAOORI,
+IIF(VlpStspag = 6 AND VLPIDCREDITTRANSACTIONPAI > 0, SUM(VlpVlrPag), 0) VALORCESSAOBNF,
+IIF(VlpAnpNum > 0, SUM(VLPVLRORIPAG - VlpVlrPag), 0) CUSTOANT,
+0 PAGOBNF
+FROM VLRPAG
+WHERE EstCod = @ESTCOD AND VlpNsu = 0
+GROUP BY VlpMovTrnId, VlpStspag, VlpIdCreditTransaction, VlpIdCreditTransactionPai, VlpAnpNum
+) B
+ON A.MovTrnId = B.VlpMovTrnId
+WHERE A.EstCod = @ESTCOD AND MovTrnNsu = 0
+/*INSERT ANALITICO AJUSTES - FIM*/
+
+/*INSERT CONSOLIDADO - INICIO*/
+SELECT @MOVTRNVLRVENDA = COALESCE(SUM(MovTrnVlr), 0), @MOVTRNVLRLIQVENDA = COALESCE(SUM(MovTrnVlrLiqEst),0) 
+FROM MovTrn01 WHERE EstCod = @ESTCOD AND MovTrnCod <> 'CC';
+
+SELECT @MOVTRNVLRCANC = COALESCE(SUM(MovTrnVlr),0), @MOVTRNVLRLIQCANC = COALESCE(SUM(MovTrnVlrLiqEst),0) 
+FROM MovTrn01 WHERE EstCod = @ESTCOD AND MovTrnCod = 'CC';
+
+SELECT @TOTALANT = COALESCE(SUM(RtAnaliticoValorAnt),0), @TOTALABERTO = COALESCE(SUM(RtAnaliticoValorAberto),0),
+@TOTALDETCESSAOORI = COALESCE(SUM(RtAnaliticoCessao),0), @TOTALDETCESSAOBEN = COALESCE(SUM(RtAnaliticoCessaoBnf),0),
+@TOTALPAGO = COALESCE(SUM(RtAnaliticoVALORPAGO),0), @TOTALPAGOBNF = COALESCE(SUM(RtAnaliticoVALORPAGOBNF),0)
+FROM RtAnalitico
+WHERE RtAnaliticoEstCod = @ESTCOD;
+
+SELECT @TOTALCESSAOORI = COALESCE(SUM(CessaoValor),0) 
+FROM CESSAO
+WHERE CessaoStatusId = 1
+AND CessaoEstCodOri = @ESTCOD;
+
+SELECT @TOTALCESSAOBEN = COALESCE(SUM(CessaoValor),0) 
+FROM CESSAO
+WHERE CessaoStatusId = 1
+AND CessaoEstCodBen = @ESTCOD;
+
+SELECT @TOTALARQUIVO = COALESCE(SUM(ArbDetVlr),0) 
+FROM ARQDET
+WHERE ArbDetCodSit IN (0, 4)
+AND ArbDetEstCod = @ESTCOD;
+
+INSERT INTO RtConsolidado (RTCONSOLIDADOESTCOD, RTCONSOLIDADOVENDAVALOR, RTCONSOLIDADOLIQESTVALOR, RTCONSOLIDADOABERTOVALOR,
+RTCONSOLIDADOCANCELAMENTOVALOR, RTCONSOLIDADORECEBERVALOR, RTCONSOLIDADOCESSAOORIVALOR, RtConsolidadoCessaoDETOriValor,
+RTCONSOLIDADOCESSAOBENVALOR, RtConsolidadoCessaoDETBenValor, RTCONSOLIDADOBANCOVALOR, RTCONSOLIDADOCUSTOANTVALOR, RTCONSOLIDADOPAGO,RTCONSOLIDADOGUID)
+VALUES (
+@ESTCOD, @MOVTRNVLRVENDA, @MOVTRNVLRLIQVENDA, @TOTALABERTO, @MOVTRNVLRLIQCANC, (@MOVTRNVLRLIQVENDA + @MOVTRNVLRLIQCANC), 
+@TOTALCESSAOORI, @TOTALDETCESSAOORI, @TOTALCESSAOBEN, @TOTALDETCESSAOBEN, @TOTALARQUIVO, @TOTALANT, (@TOTALPAGO + @TOTALPAGOBNF),
+@RELGUID
+)
+/*INSERT CONSOLIDADO - FIM*/
+END
+GO
+
+
