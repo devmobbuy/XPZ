0846784b52893727bec5fa8153f2f6f60a9dac30
Wesley Emidio <wesley@dngx.com.br>
2023-01-02 12:16:03 -0300

    tarefa cessao cancelada

---
+++ Scripts/Ver_QA/14_#20504_NovaTela_Wesley_02012023.sql
@@ -0,0 +1,12 @@
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('CessaoCancMotGrid', 'CessaoCancMotGrid', 'Motivo Cancelamento Cessão', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'CessaoCancMotGrid', '', 103, 'TAB_GER', '', '/pronto/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (25, 'CessaoCancMotGrid')
+
+--Insere tela no grupo
+INSERT INTO sse2_grp_mod
+VALUES (25, 'ADM', 'CessaoCancMotGrid')
