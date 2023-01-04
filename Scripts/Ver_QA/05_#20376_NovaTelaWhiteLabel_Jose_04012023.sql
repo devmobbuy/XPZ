ae7b2a93bc46d8559b4415ca52af8d17ba8c27ce
José Luiz Lemos da Silva <jose@dngx.com.br>
2023-01-04 12:16:31 -0300

    Novo script

---
+++ Scripts/Ver_QA/05_#20376_NovaTelaWhiteLabel_Jose_04012023.sql
@@ -0,0 +1,14 @@
+/* TAREFA #20376 - JOSÉ */
+
+--Rodar script em todos os clientes que irá ser atualizada a versão
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('CadastroWhitelabel', 'CadastroWhitelabel', 'Cadastro de whitelabel', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'CadastroWhitelabel', '', 161, 'TAB_GER', '', '/pronto/servlet/') --Mudar onde está 'pronto' conforme cliente
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (25, 'CadastroWhitelabel') --Verificar a unidade de negócio para modificar de '25' se for diferente da Pronto
+
+INSERT INTO sse2_grp_mod
+VALUES (25, 'ADM', 'CadastroWhitelabel') --Verificar a unidade de negócio para modificar de '25' se for diferente da Pronto
