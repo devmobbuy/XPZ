b4eb2b16d414def9394ae48120061d91b07d50c6
Wesley Emidio <wesley@dngx.com.br>
2023-01-10 17:04:14 -0300

    Tarefa transacoes pix

---
+++ Scripts/Ver_QA/13_#20552_ParsisCriados_Wesley_10012023.sql
@@ -0,0 +1,48 @@
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_HOST'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_BASEURL'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_CODSUBEMPRESA'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_CLIENTID'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_USUARIO'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_SENHA'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_Pix_ESPECIFICA'
+--SELECT ParCon FROM PARSIS WHERE PARCOD = 'ADI_PIX_DATA_PROCESS'
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_HOST', 'Host da API de transações PIX', 'CA', 80, 'N', 'www.mobbuyapp.com:10443')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_BASEURL', 'BaseUrl da API de transações PIX', 'CA', 80, 'N', '/InterfaceManagerMobbuy/interface/processos/apipix/')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_CODSUBEMPRESA', 'CodSubEmpresa da API de transações PIX', 'CA', 80, 'N', '1002')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_CLIENTID', 'ClientId da API de transações PIX', 'CA', 80, 'N', 'c11863329d7eca1d8f8172a9578ae9f5')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_USUARIO', 'Usuario da API de transações PIX', 'CA', 80, 'N', 'mobbuyguest')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_SENHA', 'Senha da API de transações PIX', 'CA', 80, 'N', '8f543df1d7571e92db14b5fa067552e6')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_Pix_ESPECIFICA', 'Tem data especifica para rodar da API de transações PIX ? ', 'CA', 80, 'N', 'S')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ADI_PIX_DATA_PROCESS', 'Data especifica da API de transações PIX (AAA-MM-DD)', 'CA', 80, 'N', '2022-07-01')
---
+++ Scripts/Ver_QA/14_#20552_TabelaTrn12_Wesley_10012023.sql
@@ -0,0 +1,27 @@
+--drop table [Trn12]
+
+CREATE TABLE [Trn12] (
+  [Transaction_pix]         DECIMAL(18)    NOT NULL,
+  [Dt_hr_trans]             DATETIME    NULL,
+  [Cd_company]              DECIMAL(18)    NULL,
+  [Cd_partner]              SMALLINT    NULL,
+  [Cnpj_terminal]           VARCHAR(14)    NULL,
+  [Cd_terminal_physical]    VARCHAR(10)    NULL,
+  [Pay_value]               DECIMAL(17,2)    NULL,
+  [Pay_liquid]              DECIMAL(17,2)    NULL,
+  [Quantity_parcel]         SMALLINT    NULL,
+  [Dt_hr_inclusao_cobranca] DATETIME    NULL,
+  [Txid_cobranca]           VARCHAR(40)    NULL,
+  [Valor_cobranca]          DECIMAL(17,2)    NULL,
+  [Valor_tarifa_cobranca]   DECIMAL(17,2)    NULL,
+  [Situacao_cobranca]       VARCHAR(20)    NULL,
+  [Dt_hr_pagamento]         DATETIME    NULL,
+  [Valor_pagamento]         DECIMAL(17,2)    NULL,
+  [Nsu_pagamento]           VARCHAR(20)    NULL,
+  [Valor_tarifa_pagamento]  DECIMAL(17,2)    NULL,
+  [Situacao_pagamento]      VARCHAR(20)    NULL,
+  [Loc_Num_Log]             DECIMAL(18)    NULL,
+  [Status_Processamento]    VARCHAR(40)    NULL,
+  [Dt_hr_Importacao]        DATETIME    NULL,
+  [Json_Entrada]            VARCHAR(MAX)    NULL,
+     PRIMARY KEY ( [Transaction_pix] ))
