7aca5b10b99225b68a1d2809f38e08b5428afbdb
Wesley Emidio <wesley@dngx.com.br>
2023-01-13 17:22:11 -0300

    parsis circularizacao

---
+++ Scripts/Ver_QA/13_#20420_ParsisCircularizacao_Wesley_13012023.sql
@@ -0,0 +1,9 @@
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('ANOINICIO_CIRCULARIZACAO', 'Ano que começou a valer a carta de circularização', 'CA', 80, 'N', '2022')
+
+INSERT INTO PARSIS 
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon)
+VALUES
+('MESINICIO_CIRCULARIZACAO', 'Mês que começou a valer a carta de circularização', 'CA', 80, 'N', '12')
