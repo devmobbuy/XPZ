55f770f31c11ea7937be71144a1657e6c3ee13f0
José Luiz Lemos da Silva <jose@dngx.com.br>
2023-01-23 10:30:27 -0300

    Novo script

---
+++ Scripts/Ver_QA/23_#20867_NovoParametroAntecipacaoNegativo_Jose_23012023.sql
@@ -0,0 +1,5 @@
+/* TAREFA #20867 - JOSÉ */
+
+--Rodar script em todos os clientes que for ser atualizada a versão
+INSERT INTO PARSIS
+VALUES ('ANT_CONSIDERA_NEGATIVO', 'Considera valor negativo (Ajuste e Cancelamento) na antecipação ou não', 'VA', 40, NULL, 'N', 'N', 'ADMIN', '2023-01-23 10:19:57.000', NULL, NULL, 0);
