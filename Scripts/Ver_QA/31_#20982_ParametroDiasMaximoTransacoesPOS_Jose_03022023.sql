7af56e981bc700a592ace7f95968eac4402ba7f5
José Luiz Lemos da Silva <jose@dngx.com.br>
2023-02-03 15:27:26 -0300

    Novo script

---
+++ Scripts/Ver_QA/31_#20982_ParametroDiasMaximoTransacoesPOS_Jose_03022023.sql
@@ -0,0 +1,5 @@
+/* TAREFA #20982 - JOSÉ */
+
+--Rodar script em todos os clientes que serão atualizados
+INSERT INTO PARSIS
+VALUES ('TRANSACAO_POS_DATA', 'Limite de intervalo de data para filtragem no relatório', 'NU', 1, NULL, 'N', '3', 'ADMIN', '2023-02-03 09:37:36.000', NULL, NULL, 0)
