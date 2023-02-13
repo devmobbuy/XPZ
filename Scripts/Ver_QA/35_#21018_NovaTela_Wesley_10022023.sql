5ad7f861bac549207102eb517af3a3115b33864f
Wesley Emidio <wesley@dngx.com.br>
2023-02-13 12:26:07 -0300

    nome menu

--- Scripts/Ver_QA/35_#21018_NovaTela_Wesley_10022023.sql
+++ Scripts/Ver_QA/35_#21018_NovaTela_Wesley_10022023.sql
@@ -1,6 +1,6 @@
 --Rodar script em todos os clientes que irá ser atualizada a versão
 INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
-VALUES ('SituacaoTrn05_Grid', 'SituacaoTrn05_Grid', 'Situação TRN05', '', '', 0, 1, '')
+VALUES ('SituacaoTrn05_Grid', 'SituacaoTrn05_Grid', 'Transações Registradas como Pendências', '', '', 0, 1, '')
 
 INSERT INTO sse2_mnu02 (MnucOd, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
 VALUES (10, 1, 'SituacaoTrn05_Grid', '', 10, 'REDE_CAP', '', '/pronto/servlet/') --Mudar onde está 'pronto' conforme cliente
