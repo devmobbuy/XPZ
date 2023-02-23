cdce6a8725b42591f81bfd563b5bb297c2248d0e
lvsantosdev <ldsantos.developer@gmail.com>
2023-02-23 11:56:42 -0300

    script parametro log

---
+++ Scripts/Ver_QA/28_sem_tarefa_Parametro_logtmp_Leonardo_23022023.sql
@@ -0,0 +1,41 @@
+use credpag
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/024/')
+go
+
+use Banese
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/012/')
+go
+
+use Pronto
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/025/')
+go
+
+use smartpagamentos
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/018/')
+go
+
+use Credinov
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/026/')
+go
+
+use thepay
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/027/')
+go
+
+use estbank
+go
+insert into PARSIS (parcod, ParDsc, ParTipPar, ParTamPar, ParCon)
+values ('DIR_LOG_TMP',	'Arquivo geral de log',	'CA',	300, '/mnt/home1/SubAdquirencia/028/')
+go
