d4ac8a6b1b75b3b66fc340c2ae1debdfa0165268
lvsantosdev <ldsantos.developer@gmail.com>
2023-03-20 11:32:44 -0300

    parametros para gerar dois arquivos banco credpag

---
+++ Scripts/Ver_QA/23_#21447_Parametros_arq_banco_Credpag_Leonardo_20032023.sql
@@ -0,0 +1,7 @@
+use credpag
+go
+
+insert into parsis(ParCod, ParDsc, ParTipPar, ParTamPar, ParIndSin, ParCon) 
+values
+('MUDAR_FLAG_BANCO', 'Alterar flag do banco para pagar apenas Transferencia', 'CA', 1, 'N', 'S'),
+('BANCOD_FLAG', 'Banco para fazer TED', 'CA', 100, 'N', '237')
